48c164684e282e4e7dadc97b604c3bedb9feb0c7
Abdullah Omar <fishilike.fish@gmail.com>
2023-03-14 03:49:12 +0600

    Update create-user.dto.ts

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsAlphanumeric, MaxLength } from 'class-validator';
 
 export class CreateUserDto {
   @ApiProperty()
+  @IsAlphanumeric()
+  @MaxLength(50)
   name: string;
 
   // @ApiProperty({ required: false })
